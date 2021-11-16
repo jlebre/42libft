b6ff1138d2d2107f1c8f72cf453edfd702a5cde8
jlebre <94384240+jlebre@users.noreply.github.com>
2021-11-16 18:04:06 +0000

    Add files via upload

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,36 +6,39 @@
 /*   By: jlebre <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 14:00:41 by jlebre            #+#    #+#             */
-/*   Updated: 2021/11/15 16:31:07 by jlebre           ###   ########.fr       */
+/*   Updated: 2021/11/16 18:03:13 by jlebre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(char *str)
+int	ft_atoi(const char *str)
 {
 	int	i;
 	int	val;
 	int	sinal;
 
 	i = 0;
+	val = 0;
 	sinal = 1;
 	while ((str[i] == '\n') || (str[i] == '\r') || (str[i] == '\t')
 		|| (str[i] == '\v') || (str[i] == '\f') || (str[i] == ' '))
 		i++;
 	while ((str[i] == '+') || (str[i] == '-'))
 	{
-		if (str[i] == '-')
+		if (str[i] == '-')	
 			sinal *= -1;
 		i++;
 	}
 	if ((str[i] >= 48) && (str[i] <= 57))
+	{	
 		val = (str[i] - '0');
-	i++;
-	while ((str[i] >= 48) && (str[i] <= 57))
-	{
-		val = ((val * 10) + (str[i] - '0'));
 		i++;
+		while ((str[i] >= 48) && (str[i] <= 57))
+		{
+			val = ((val * 10) + (str[i] - '0'));
+			i++;
+		}
 	}
 	return (sinal * val);
 }
