40c344df4a7fec4677962f6f6462fc2761aa81fb
jlebre <94384240+jlebre@users.noreply.github.com>
2021-12-07 19:03:59 +0000

    Add files via upload

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jlebre <jlebre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 16:16:55 by jlebre            #+#    #+#             */
-/*   Updated: 2021/12/07 16:26:03 by jlebre           ###   ########.fr       */
+/*   Updated: 2021/12/07 18:56:40 by jlebre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,22 @@
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
     t_list  *new_lst;
+    t_list  *i;
 
     if (!lst || !f)
         return (NULL);
-    while (lst)
+    new_lst = ft_lstnew(f(lst->content));
+    if (!new_lst)
+        return (NULL);
+    i = new_lst;
+    lst = lst->next;
+    while (lst && i)
     {
-        new_lst->content = f(lst->content);
-        if (!new_lst->next)
-            ft_lstclear(&new_lst, del);
+        i->next = ft_lstnew(f(lst->content));
+        if (!i->next)
+            ft_lstclear(&i, del);
         lst = lst->next;
-        new_lst = new_lst->next;
+        i = i->next;
     }
     return (new_lst);
 }
