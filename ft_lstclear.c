eb534b3c8d0c3ccf8706d9a0674bdcc084e11fb2
jlebre <94384240+jlebre@users.noreply.github.com>
2021-12-22 16:59:09 +0000

    Add files via upload

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -14,11 +14,15 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
+	t_list	*temp;
+
+	if (!*lst || !lst)
+		return ;
 	while (*lst)
 	{
-		del((*lst)->content);
-		free(*lst);
-		*lst = (*lst)->next;
+		temp = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = temp;
 	}
 	*lst = NULL;
 }
